da83a8acba9d8d5b712b629bb1dc753d6c0c680a
ReidHoneycutt <30945205+ReidHoneycutt@users.noreply.github.com>
2023-12-22 04:02:15 -0800

    Update silouetteWordCollage.js

--- silouetteWordCollage.js
+++ silouetteWordCollage.js
@@ -14,4 +14,29 @@ function draw() {
   text(random(colorlist), random(W), random(H));
 }
 
+// (1) Create image filter where the output is a set of point-pairs describing the edges of a silhouette
+function filter() {
+  // image edge-detect
+  // get point pairs
 
+}
+// (2) Calculate the number of characters per line
+function create_num_characters_array() {
+
+}
+// (3) Create mappings between the letters and the character cells
+function create_letter_character_mapping() {
+
+}
+// (4) Develop a "path" for each word
+function create_paths_for_each_word() {
+
+}
+// (5) Calculate "path" length
+function calculate_path_length() {
+
+}
+// (6) Calculate velocity from length
+function calculate_word_velocity_given_length() {
+
+}
